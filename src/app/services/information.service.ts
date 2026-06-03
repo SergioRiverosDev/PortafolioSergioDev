8258ae02815e1f0aeff0e73691ba86df847f7136
SergioRiverosDev <sergioriverosdev@gmail.com>
2026-06-03 15:54:53 -0500

    feat: add projects and information data with fallback to local JSON in services

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -2,18 +2,27 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { InformationModel } from '../models/information.model';
-import { first } from 'rxjs';
+import { catchError, first, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InformationService {
-  
+
   private url = `${environment.urlServer}/information`;
   private http = inject(HttpClient);
+  private fallbackUrl = 'database/db.json';
 
-  getInformation(){
-    return this.http.get<InformationModel>(this.url).pipe(first());
+  getInformation() {
+    return this.http.get<InformationModel>(this.url).pipe(
+      first(),
+      catchError(() => {
+        // Si Railway falla, lee el db.json local
+        return this.http.get<{ information: InformationModel }>(this.fallbackUrl).pipe(
+          map(db => db.information)
+        );
+      })
+    );
   }
 
 }
--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProjectModel } from '../models/project.model';
-import { first, pipe } from 'rxjs';
+import { catchError, first, map, pipe } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -11,8 +11,17 @@ export class ProjectsService {
 
   private url = `${environment.urlServer}/projects`
   private http = inject(HttpClient);
+  private fallbackUrl = 'database/db.json';
 
   getProjects(){
-    return this.http.get<ProjectModel[]>(this.url).pipe(first());
+    return this.http.get<ProjectModel[]>(this.url).pipe(
+      first(),
+      catchError(() => {
+        // Si Railway falla, lee el db.json local
+        return this.http.get<{ projects: ProjectModel[] }>(this.fallbackUrl).pipe(
+          map(db => db.projects)
+        );
+      })
+    );
   }
 }
